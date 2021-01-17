9ecaa1344039f447a074e253d5da896e5d04e495
vagrant <vagrant@homestead>
2021-01-17 15:52:52 +0000

    Added a google maps view on gigs show page, it shows the users where the location is of the gig

--- resources/views/admin/gigs/show.blade.php
+++ resources/views/admin/gigs/show.blade.php
@@ -9,24 +9,27 @@
             Gig: {{ $gig->bandName }} <a href="https://www.ticketmaster.ie/" class="btn btn-info float-right">Tickets</a>
           </div>
 
-                <div class="col-md-6">
-                  <img src="..." alt="...">
-                </div>
+                  <div class="col-md-6">
+                    <img src="..." alt="...">
+                  </div>
 
-                <div class="col-md-6">
-                <div class="class-body">
-                  <h5 class="card-title">{{ $gig->bandName }}</h5>
-                  <p class="card-text">Loction: {{ $gig->location }}, Co. {{ $gig->county->name }} <br> Date and Time (Y-M-D): {{ $gig->dateTime }} <br> Price: €{{ $gig->price }} <br> Genre: {{ $gig->genre }}</p>
-                  <p class="card-text"><small class="text-muted">Posted By: {{ $gig->user->name }}</small></p>
-                  <a href="{{ route('admin.gigs.index') }}" class="btn btn-default">Back</a>
-                  <a href="{{ route('admin.gigs.edit', $gig->id) }}" class="btn btn-warning">Edit</a>
-                  <form style="display:inline-block" method="POST" action="{{  route('admin.gigs.destroy', $gig->id)  }}">
-                  <input type="hidden" name="_method" value="DELETE">
-                  <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                  <button type="submit" class="form-control btn btn-danger">Delete</a>
-                  </form>
-              </div>
+                  <div class="col-md-6">
+                  <div class="class-body">
+                    <h5 class="card-title">{{ $gig->bandName }}</h5>
+                    <p class="card-text">Loction: {{ $gig->location }}, Co. {{ $gig->county->name }} <br> Date and Time (Y-M-D): {{ $gig->dateTime }} <br> Price: €{{ $gig->price }} <br> Genre: {{ $gig->genre }}</p>
+                    <p class="card-text"><small class="text-muted">Posted By: {{ $gig->user->name }}</small></p>
+                    <a href="{{ route('admin.gigs.index') }}" class="btn btn-default">Back</a>
+                    <a href="{{ route('admin.gigs.edit', $gig->id) }}" class="btn btn-warning">Edit</a>
+                    <form style="display:inline-block" method="POST" action="{{  route('admin.gigs.destroy', $gig->id)  }}">
+                    <input type="hidden" name="_method" value="DELETE">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <button type="submit" class="form-control btn btn-danger">Delete</a>
+                    </form>
+                </div>
               <br>
+            </div>
+            <div class="col-md-8">
+              <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q={{ $gig->location }}{{ $gig->county->name }}+(Gig)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
             </div>
           </div>
         </div>
