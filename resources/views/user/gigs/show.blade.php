1fe403ceffdbb16e09eb11bf0ff12c26a76ba1df
vagrant <vagrant@homestead>
2021-01-17 15:55:50 +0000

    Added the google maps to users gigs page

--- resources/views/user/gigs/show.blade.php
+++ resources/views/user/gigs/show.blade.php
@@ -22,6 +22,9 @@
               </div>
               <br>
             </div>
+            <div class="col-md-8">
+              <div style="width: 100%"><iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q={{ $gig->location }}{{ $gig->county->name }}+(Gig)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe></div>
+            </div>
           </div>
         </div>
       </div>
